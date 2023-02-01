0e3ae8df0cfdbd65f5a10a5432e40d5f872ce198
RyonMB <43067960+RyonMB@users.noreply.github.com>
2023-02-01 22:18:18 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -9,7 +9,7 @@ from datetime import datetime, timezone
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://ryonmb.github.io/tiktok-rss-flat/"
 
 count = 10
 
